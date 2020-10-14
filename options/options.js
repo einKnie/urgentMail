f649363e8b970fea896440cb790b90b5a2f5484e
lülü <einKnie@gmx.at>
2020-10-14 18:43:32 +0200

    cleanup

--- options/options.js
+++ options/options.js
@@ -1,31 +1,41 @@
 // options script
 (function() {
 
-var vd_description = "Select folders to monitor for new mail";
-var vd_version = "v0.0";
+  var um_description = "Select folders to monitor for new mail";
+  var um_version = "v0.0";
 
-// add listsners.
-// need three listeners instead of one, because i don't want the button inside the form.
-// need to manually 'disalke' the default action of form-submit -> reloads page, which is unnecessary.
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("applybutton").addEventListener("click", saveOptions);
-document.querySelector("form").addEventListener('submit', function(e) {e.preventDefault();});
+  const DEBUG = true;
+  var logDebug;
+  if (DEBUG) {
+    logDebug = console.log;
+  } else {
+    logDebug = function () { };
+  }
 
-getVersion();
+  // add listeners.
+  // need to manually 'disable' the default action of form-submit -> reloads page, which is unnecessary.
+  document.addEventListener("DOMContentLoaded", restoreOptions);
+  document.getElementById("applybutton").addEventListener("click", saveOptions);
+  document.querySelector("form").addEventListener('submit', function(e) {e.preventDefault();});
 
-/*
-* Helper to store new settings
-*/
-function fetchSettings(result) {
-  return new Promise((resolve,reject) => {
-    console.log(result);
-    var newprefs = result; // local storage
-    try {
-      for (a of newprefs.accounts) {
-         // for each account
-         console.log(a);
-         for (folder of a.folders) {
-            // for each folder
+  getVersion();
+
+  /*
+  * Helper to store new settings
+  * takes stored settings as argument,
+  * updates local settings from settings page,
+  * then returns promise for updated settings
+  */
+  function fetchSettings(result) {
+    logDebug("in fetchSettings");
+    return new Promise((resolve, reject) => {
+      logDebug(result);
+      var newPrefs = result; // local storage
+      try {
+        for (a of newPrefs.accounts) {
+          logDebug("checking account: ");
+          logDebug(a);
+          for (folder of a.folders) {
             let chkbox = document.getElementById(`${a.id.accountId}/${folder.path}`);
             if (chkbox.checked) {
               // check if folder in monitored and add
@@ -33,7 +43,7 @@ function fetchSettings(result) {
                 a.monitored.push(folder.path);
               }
             } else {
-              // check if folder in monitores, and remove
+              // check if folder in monitored, and remove
               if (a.monitored.includes(folder.path)) {
                 for (let i = 0; i < a.monitored.length; i++) {
                   if (a.monitored[i] == folder.path) {
@@ -44,144 +54,147 @@ function fetchSettings(result) {
             }
           }
         }
-    } catch(e) {
-      reject(`error fetching values: ${e}`);
-   }
-   resolve(newprefs);
- });
-}
+      } catch(e) {
+        reject(`error fetching settings: ${e}`);
+      }
+      resolve(newPrefs);
+    });
+  }
 
-/*
-* Get current addon version from manifest.json
-*/
-function getVersion() {
- var man = browser.runtime.getManifest();
- if (man.hasOwnProperty("version")) {
-   vd_version = `v${man.version}`;
- }
-}
+  /*
+  * Get current addon version from manifest.json
+  */
+  function getVersion() {
+    var man = browser.runtime.getManifest();
+    if (man.hasOwnProperty("version")) {
+      um_version = `v${man.version}`;
+    }
+  }
 
-/*
-* Store options from settings page
-*/
-function saveOptions(e) {
- e.preventDefault();
+  /*
+  * Store options from settings page
+  */
+  function saveOptions(e) {
+    e.preventDefault();
 
- var newSites = browser.storage.local.get(["accounts"]);
- newSites.then(fetchSettings, onError).then(function(result) {
-   console.log("storing data");
-     return new Promise((resolve, reject) => {
-       console.log(result);
-       var newprefs = {
-         accounts: result.accounts
-       };
-       console.log(newprefs);
-       browser.storage.local.set(newprefs)
-        .then(function(){resolve("yay");}, function(){reject("Failed to store data");});});
-   }, onError);
-}
+    var oldPrefs = browser.storage.local.get(["accounts"]);
+    oldPrefs.then(fetchSettings, onError).then(function(result) {
+      logDebug("storing new data");
+      return new Promise((resolve, reject) => {
+        logDebug(result);
+        var newPrefs = {
+          accounts: result.accounts
+        };
+        logDebug(newPrefs);
+        browser.storage.local.set(newPrefs)
+          .then(function() { resolve("yay"); }, function() { reject("Failed to store data"); });});
+      }, onError);
+    }
 
-/*
-* Apply current options to settings page
-*/
-function restoreOptions() {
+    /*
+    * Apply current options to settings page
+    */
+    function restoreOptions() {
 
- function setupSettingsPage(result) {
-   var accs = document.getElementById("accounts");
-   console.log(accs);
-   console.log(result);
-   for (a of result.accounts) {
-      console.log(a);
-      if (accs.querySelector(`div[id="${a.id.accountId}"]`) != null) {
-       // just update value in case the checkbox already exists
-       console.log("TODO: update values if html stuff exists");
+      function setupSettingsPage(result) {
+        var accs = document.getElementById("accounts");
+        if (accs == null) {
+          onError("failed to get accout container from html");
+          return;
+        }
 
-       var account = document.getElementById(`${a.id.accountId}`);
-       console.log(account);
-       var content = document.getElementById(`${a.id.accountId}/con`);
-       console.log(content);
+        for (a of result.accounts) {
+          logDebug("setup account html: ");
+          logDebug(a);
 
-       for (folder of a.folders) {
-         var chkbox = document.getElementById(`${a.id.accountId}/${folder.path}`);
-         if (chkbox == null) {
-           // new folder? create (TODO: make function out of this)
-           console.log("creating new folder")
-           var innerdiv = document.createElement("div");
-           var chkbox = document.createElement("input");
-           chkbox.type = "checkbox";
-           chkbox.id = `${a.id.accountId}/${fol.path}`;
-           chkbox.checked = (a.monitored.includes(fol.path));
-           var label = document.createElement("label");
-           label.htmlFor = chkbox;
-           label.appendChild(document.createTextNode(`Monitor ${fol.name}`));
-           innerdiv.appendChild(chkbox);
-           innerdiv.appendChild(label);
-           content.appendChild(innerdiv);
-         } else {
-           if (a.monitored.includes(folder.path)) {
-             chkbox.checked = true;
-           } else {
-             chkbox.checked = false;
-           }
-         }
-       }
+          if (document.getElementById(`${a.id.accountId}/con`) != null) {
+            // just update values in case the checkbox already exists
+            // .. does this ever actually happen?
+            logDebug("updating existing html");
 
-       console.log(a);
+            var content = document.getElementById(`${a.id.accountId}/con`);
+            logDebug(content);
+
+            for (folder of a.folders) {
+              var chkbox = document.getElementById(`${a.id.accountId}/${folder.path}`);
+              if (chkbox == null) {
+                content.appendChild(createCheckbox(a, folder));
+                chkbox = document.getElementById(`${a.id.accountId}/${folder.path}`);
+              }
+
+              if (a.monitored.includes(folder.path)) {
+                chkbox.checked = true;
+              } else {
+                chkbox.checked = false;
+              }
+            }
+          } else {
+            // add account
+            var account = document.createElement("div");
+            account.id  = a.id.accountId;
+
+            var button = document.createElement("button");
+            button.id         = `${a.id.accountId}/btn`;
+            button.classname  = "collapsible";
+            button.innerHTML  = a.id.name;
+            button.addEventListener("click", onButtonToggle);
+
+            var content = document.createElement("div");
+            content.classname = "content";
+            content.id        = `${a.id.accountId}/con`;
+            content.style.display = "none";
+
+            for (fol of a.folders) {
+              content.appendChild(createCheckbox(a, fol));
+            }
+
+            account.appendChild(button);
+            account.appendChild(content);
+            accs.appendChild(account);
+          }
+        }
+
+        document.getElementById("apptext").textContent    = um_description;
+        document.getElementById("appversion").textContent = um_version;
+      }
+
+      browser.storage.local.get(["accounts"])
+      .then(setupSettingsPage, onError);
+    }
+
+    /*
+     * Create a checkbox on the settings page
+     */
+    function createCheckbox(a, fol) {
+      var innerdiv = document.createElement("div");
+      var chkbox = document.createElement("input");
+      chkbox.type = "checkbox";
+      chkbox.id = `${a.id.accountId}/${fol.path}`;
+      chkbox.checked = (a.monitored.includes(fol.path));
+
+      var label = document.createElement("label");
+      label.htmlFor = chkbox.id;
+      label.appendChild(document.createTextNode(`Monitor ${fol.name}`));
+
+      innerdiv.appendChild(chkbox);
+      innerdiv.appendChild(label);
+      return innerdiv;
+    }
+
+    /*
+     * handler for expand button
+     */
+    function onButtonToggle(e) {
+      let content = e.target.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
       } else {
-       // add accounts
-       var elem = document.createElement("div");
-       elem.id = a.id.accountId;
-       var button = document.createElement("button");
-       console.log(button);
-       button.id = `${a.id.accountId}/btn`;
-       button.classname = "collapsible";
-       button.innerHTML = a.id.name;
-       button.addEventListener("click", onButtonToggle);
-       var content = document.createElement("div");
-       content.classname = "content";
-       content.id = `${a.id.accountId}/con`;
-       content.style.display = "none";
-
-       for (fol of a.folders) {
-         var innerdiv = document.createElement("div");
-         var chkbox = document.createElement("input");
-         chkbox.type = "checkbox";
-         chkbox.id = `${a.id.accountId}/${fol.path}`;
-         chkbox.checked = (a.monitored.includes(fol.path));
-         var label = document.createElement("label");
-         label.htmlFor = chkbox.id;
-         label.appendChild(document.createTextNode(`Monitor ${fol.name}`));
-         innerdiv.appendChild(chkbox);
-         innerdiv.appendChild(label);
-         content.appendChild(innerdiv);
-       }
-
-       elem.appendChild(button);
-       elem.appendChild(content);
-       accs.appendChild(elem);
+        content.style.display = "block";
       }
     }
 
-   document.getElementById("apptext").textContent    = vd_description;
-   document.getElementById("appversion").textContent = vd_version;
- }
+    function onError(error) {
+      console.log(`Error: ${error}`);
+    }
 
- browser.storage.local.get(["accounts"])
-  .then(setupSettingsPage, onError);
-}
-
-function onButtonToggle(e) {
-  // console.log(e);
-  let content = e.target.nextElementSibling;
-  if (content.style.display === "block") {
-    content.style.display = "none";
-  } else {
-    content.style.display = "block";
-  }
-}
-
-function onError(error) {
- console.log(`Error: ${error}`);
-}
-
-})();
+  })();
